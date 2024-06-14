d7baebd8e113dce980d3a5b814205201e61a8266
Jean <rjeandelince@gmail.com>
2024-06-14 06:46:58 +0200

    Ajout du fav icon

--- Views/partials/header.php
+++ Views/partials/header.php
@@ -1,6 +1,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="og:title"
+        content="Association Michel Archange Zero Chômage, Aides aux personnes âgées, Aides aux personnes handicapées, aides aux professionnels, aides juridiques, organisation d'événement, débats réligieux" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link
@@ -11,6 +13,7 @@
     <link rel="stylesheet" href="../../Assets/css/style.css">
     <link rel="stylesheet" href="../../Assets/css/style-admin.css">
 
+    <link rel="shortcut icon" type="image/png" href="../../Assets/images/favicon.png">
 
     <title>
         <?= $title ?>
